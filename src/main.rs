48c6dedd8a8c6a91b7224c6fb6e87e830cdcef0e
ZhangShu <zhangshuyx@gmail.com>
2021-07-11 18:51:45 +0800

    Update main.rs
    aaa

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,17 @@ fn main() {
     let s1 = String::from("hello");
     let s2 = s1.clone();
     println!("s1 = {}, s2 = {}", s1, s2);
+
+
+    let mut s = String::from("hello");
+
+    let r1 = &s; // no problem
+    let r2 = &s; // no problem
+    println!("{} and {}", r1, r2);
+    // r1 and r2 are no longer used after this point
+
+    println!("{} and {}", r1, r2);
+
+    let r3 = &mut s; // no problem
+    println!("{}", r3);
 }
